ee2666e437eaed0a901c0ce10f2685f4d26b5e54
sainurdin <sainurdin.com>
2015-11-14 15:00:35 +0700

    Sound Effect Successful

--- app/src/main/java/bu/sainurdin/butreaffic/MainActivity.java
+++ app/src/main/java/bu/sainurdin/butreaffic/MainActivity.java
@@ -1,13 +1,51 @@
 package bu.sainurdin.butreaffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+    } //Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.lion);
+                buttonPlayer.start();
+
+                //Intent to WebView
+
+
+            } // event
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+
+} //Main Class
